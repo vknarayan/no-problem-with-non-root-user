bab7fd822cac26ca2198b75b58cec392cf8e34e3
Vasanthakumar Narayan <37932621+vknarayan@users.noreply.github.com>
2019-06-04 15:38:43 +0530

    unnecessary options removed for useradd, chown

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,11 @@
 USER_ID=${host_UID:-9001}
 
 # Add a new user with UID passed in the run command
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m orange
-HOME=/home/orange
+useradd --shell /bin/bash -u $USER_ID -o orange
 
 #change ownership of the mounted volume in the container
-chown -R orange:orange /usr/share/filebeat
+chown -R orange /usr/share/filebeat
 
-exec /usr/local/bin/gosu orange "$@"
+exec /usr/local/bin/gosu orange filebeat
 
 
